a710cecd3b0e9480d5c7fac6b520c5decf2ce1f4
Grimmow <64664662+Grimmow@users.noreply.github.com>
2024-01-27 15:41:48 +0200

    Number ticker animation on score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
 
 public class Score : MonoBehaviour
 {
     TextMeshProUGUI text;
     public int score = 0;
+    int prescore;
     private void Start() {
         text = GetComponent<TextMeshProUGUI>();
+        Display();
     }
 
     public void IncreaseScoreBy(int points) {
         score += points;
-        text.text = "SCORE \n" + score.ToString("00000000");
+        //text.text = "SCORE \n" + score.ToString("00000000");
+    }
+
+    void Display() {
+        DOVirtual.Int(prescore, score, 0.2f, (x) => {
+            prescore = x;
+            text.text = "SCORE \n" + prescore.ToString("00000000");
+        }).OnComplete(()=> { Display(); });
     }
 }
