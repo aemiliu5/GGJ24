dfb2224f1238a2ed79ee8bd5cef0b704e9220fce
KonstantinAth <106696362+KosCreate@users.noreply.github.com>
2024-01-27 18:27:02 +0200

    main menu fin

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,12 +4,13 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour {
     [SerializeField] private string mainSceneName;
     [SerializeField] private CanvasGroup creditsCanvasGroup;
+    [SerializeField] private float creditCanvasGroupFadeDuration = 1.0f;
     public void LoadMainScene() { SceneManager.LoadScene(mainSceneName); }
     public void QuitGame() { Application.Quit(); }
     public void FadeCanvasGroup(bool fade) {
         float target = fade ? 0.0f : 1.0f;
         creditsCanvasGroup.interactable = !fade;
         creditsCanvasGroup.blocksRaycasts = !fade;
-        creditsCanvasGroup.DOFade(target, 1.0f);
+        creditsCanvasGroup.DOFade(target, creditCanvasGroupFadeDuration);
     }
 }
